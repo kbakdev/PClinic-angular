17d7cc201f3a87c5a13a3896fa935146af4ecb72
53jk1 <bonczalke@gmail.com>
2021-01-22 13:53:00 +0100

    0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PClinic-angular';
+  title: string = 'PClinic-angular';
+  name : string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DeviceComponent } from './components/device/device.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceComponent
   ],
   imports: [
     BrowserModule
